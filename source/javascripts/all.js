90da857bab79f6c1bda6119314515d3dca3f2f25
MaximeShhaker <maximelegoueff@macbook-air-de-maxime.home>
2016-01-01 20:17:45 +0100

    Added jQuery
    jQuery
       scroll reveal
       smooth scroll
       navbar background
    homepage:
       navigation angle downs
       Modal form on homepage
       first row text in bootstrap grid

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,36 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+  //change navbar background
+
+  $(window).on('scroll', function(){
+
+    if ( $(this).scrollTop() > 120 ){
+      $(".navbar").css("background-color", "black");
+    } else {
+      $(".navbar").css("background-color", "transparent");
+    }
+
+  });
+
+
+
+  //smooth scroll
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+});
